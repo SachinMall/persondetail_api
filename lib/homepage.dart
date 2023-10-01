1f7f6dfa3d3c44dc07b78c29b926aa9cc98f9279
MallSachin2524 <137764841+MallSachin2524@users.noreply.github.com>
2023-10-01 20:35:21 +0530

    initial

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -43,73 +43,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          elevation: 0,
-          title: Text("BioData"),
-          centerTitle: true,
-        ),
-        body: isloading
-            ? Center(child: CircularProgressIndicator(),) 
-            : PersonDetailList.isEmpty
-                ? Text("No Data")
-                : ListView.builder(
-                    itemCount: PersonDetailList.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.all(5),
-                        padding: EdgeInsets.all(10),
-                        height: 255,
-                        color: Colors.grey.withOpacity(0.1),
-                        child: Row(
-                          children: [
-                            Image.network(
-                              PersonDetailList[index].image.toString(),
-                              fit: BoxFit.contain,
-                              width: 100,
-                              height: 100,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        title: const Text("BioData"),
+        centerTitle: true,
+      ),
+      body: isloading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : PersonDetailList.isEmpty
+              ? const Text("No Data")
+              : ListView.builder(
+                  itemCount: PersonDetailList.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.all(10),
+                      height: 255,
+                      color: Colors.grey.withOpacity(0.1),
+                      child: Row(
+                        children: [
+                          Image.network(
+                            PersonDetailList[index].image.toString(),
+                            fit: BoxFit.contain,
+                            width: 100,
+                            height: 100,
+                          ),
+                          const SizedBox(
+                            width: 7,
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    "Name: ${PersonDetailList[index].name.toString()}"),
+                                height5,
+                                Text(
+                                    "Username: ${PersonDetailList[index].username.toString()}"),
+                                height5,
+                                Text(
+                                    "Contact: ${PersonDetailList[index].phone}"),
+                                height5,
+                                Text(
+                                    "Email: ${PersonDetailList[index].email.toString()}"),
+                                height5,
+                                Text(
+                                  "Address: ${PersonDetailList[index].address!.street}, ${PersonDetailList[index].address?.suite}, ${PersonDetailList[index].address?.city}, ${PersonDetailList[index].address?.zipcode}",
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                height5,
+                                Text(
+                                    "GeoLocation:- Latitude : ${PersonDetailList[index].address?.geo?.lat} , Lontitude : ${PersonDetailList[index].address?.geo?.lng}"),
+                                height5,
+                                Text(
+                                    "website: ${PersonDetailList[index].website}"),
+                                height5,
+                                Text(
+                                    "Organisation Details:- Org. Name: ${PersonDetailList[index].company?.name.toString()} , catchPhrase: ${PersonDetailList[index].company?.catchPhrase.toString()} , Bs: ${PersonDetailList[index].company?.bs}"),
+                              ],
                             ),
-                            SizedBox(
-                              width: 7,
-                            ),
-                            Expanded(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                      "Name: ${PersonDetailList[index].name.toString()}"),
-                                  height5,
-                                  Text(
-                                      "Username: ${PersonDetailList[index].username.toString()}"),
-                                  height5,
-                                  Text(
-                                      "Contact: ${PersonDetailList[index].phone}"),
-                                  height5,
-                                  Text(
-                                      "Email: ${PersonDetailList[index].email.toString()}"),
-                                  height5,
-                                  Text(
-                                    "Address: ${PersonDetailList[index].address?.street}, ${PersonDetailList[index].address?.suite}, ${PersonDetailList[index].address?.city}, ${PersonDetailList[index].address?.zipcode}",
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  height5,
-                                  Text(
-                                      "GeoLocation:- Latitude : ${PersonDetailList[index].address?.geo?.lat} , Lontitude : ${PersonDetailList[index].address?.geo?.lng}"),
-                                  height5,
-                                  Text(
-                                      "website: ${PersonDetailList[index].website}"),
-                                  height5,
-                                  Text(
-                                      "Organisation Details:- Org. Name: ${PersonDetailList[index].company?.name.toString()} , catchPhrase: ${PersonDetailList[index].company?.catchPhrase.toString()} , Bs: ${PersonDetailList[index].company?.bs}"),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ));
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+    );
   }
 }
